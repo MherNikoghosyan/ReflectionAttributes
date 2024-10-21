95dc072bddd6b91aac1a06cfa60284ccea5f709d
MherNikoghosyan <mher.nikoghosyan1988@gmail.con>
2024-10-21 10:49:54 +0400

    Add project files.

---
+++ ReflectionAttributes/Extenshions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionAttributes
+{
+    static class Extenshions
+    {
+       public static MethodInfo? GetOneParameterMethod(this Type type, string methodName)
+       {
+            foreach (MethodInfo item in type.GetMethods())
+            {
+                if (item.Name.Equals(methodName, StringComparison.OrdinalIgnoreCase) &&
+                    item.GetParameters().Length == 1)
+                    return item;
+            }
+            return null;
+       }
+    }
+}
---
+++ ReflectionAttributes/Program.cs
@@ -0,0 +1,51 @@
+﻿//RTTI -> runtime type information
+using System.Reflection;
+
+namespace ReflectionAttributes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int arg = 100;
+            string s = $"Sqrt({arg})";
+            string methodName = s.Substring(0, s.IndexOf('('));
+            
+            // Step 1: Get type of Class (Struct) wich method we are going to call
+            Type type = typeof(Math);
+
+            // Step 2: Finde the method by name
+            //MethodInfo? mi = type.GetOneParameterMethod(methodName);
+            MethodInfo? mi = type.GetMethod(methodName);
+
+            if (mi == null)
+            {
+                Console.WriteLine("Methot not found ");
+                return;
+            }
+            
+            // Step 3 : Call the method (via methodinfo class)
+            // arguments of invoke :
+            //   1.instance for wich the method is called // null for static methods
+            //   2.list of argument of thet method
+
+            object? rezult = mi.Invoke(null, new object[] { arg });
+
+            double ? d = rezult as double?;
+            Console.WriteLine(d);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
